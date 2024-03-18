5a9ccc17fffd4d50fa5dcb2bfa3145e45681286d
mayambacode <bokambma@sheridancollege.ca>
2024-03-18 01:18:36 -0400

    d

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,29 +5,14 @@ import { useState } from 'react';
 
 function Input() {
 
-  const [prompt, setPrompt] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    axios // calls the node server
-    .post("http://localhost:3000/chat", {prompt})
-    .then((res) => {
-      setResponse(res.data); 
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  };
-
 
   return (
     <div className='input'>
       <div> 
         {/* This is calls the function */}
-        <form onSubmit={handleSubmit}>   
+        <form >   
         {/* the function where the message will be send and you will recieve response */}
-        <input type='text' placeholder='Type a message...' value={prompt} onChange={(e) => setPrompt(e.target.value)} /> 
+        <input type='text' placeholder='Type a message...'  /> 
         <button>Send</button>
         </form>
       </div>
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -15,3 +15,5 @@ function Messages() {
     </div>
   );
 }
+
+export default Messages;
