633d8778a03c78001b1e99882b153681e1d3a92f
mayambacode <bokambma@sheridancollege.ca>
2024-03-18 01:21:42 -0400

    Revert "Worked on the message component. It will be added to the Mesages component and then into the Chat component page"
    This reverts commit 3f1b585df76e8cd9226c22da573b6e3fa3a36867.

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,15 +7,8 @@ function Input() {
 
 
   return (
-    <div className='input'>
-      <div> 
-        {/* This is calls the function */}
-        <form >   
-        {/* the function where the message will be send and you will recieve response */}
-        <input type='text' placeholder='Type a message...'  /> 
-        <button>Send</button>
-        </form>
-      </div>
+    <div className='message'>
+      <p>Message Input</p>
     </div>
   );
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,1 @@
 import React from 'react';
-
-function Message() {
-  return (
-    <div className='message'>
-      <div className='messageInfo'>
-      {/* <img />   profile pic */}
-        <span>Time</span>
-        <div className='messageContent'>
-            Message
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Message;
